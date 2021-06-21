bfe7eff84b245407a065cb42b01ab7109a1837dd
Wayne <urrightguy@gmail.com>
2021-06-21 23:48:32 +0800

    feat: create new booking function

--- routes/modules/booking.js
+++ routes/modules/booking.js
@@ -5,9 +5,20 @@ const router = express.Router()
 // 引用 Todo model
 const Record = require('../../models/Record')
 
-// route setting for create new expense booking
+// route setting for create view
 router.get('/new', (req, res) => {
   return res.render('new')
+})
+
+// route setting for new expense booking creating
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const category = req.body.category
+  const date = req.body.date
+  const amount = req.body.amount
+  return Record.create({ name, category, date, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // route setting for create new expense booking
