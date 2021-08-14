f6f6e84728b054179712fd9c11276089e949e9e6
Wayne <urrightguy@gmail.com>
2021-08-14 17:14:20 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,15 @@
 const express = require('express')
 const router = express.Router()
 
-// 引入 home 模組程式碼
-const home = require('./modules/home')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
-
-// 引入 restaurants 模組程式碼
 const booking = require('./modules/booking')
-router.use('/booking', booking)
-
-// 引入 filter 模組程式碼
 const filter = require('./modules/filter')
+const users = require('./modules/users')
+const home = require('./modules/home')
+
+router.use('/booking', booking)
 router.use('/filter', filter)
+router.use('/users', users)
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router
