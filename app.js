1eb37351f7de39830be204762c9c8dd0bca221a9
Wayne <urrightguy@gmail.com>
2021-08-14 17:38:09 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // include packages and define server related variables
 const express = require('express')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -12,29 +13,20 @@ const helpers = hbshelpers()
 const routes = require('./routes')
 
 
-// setting template engine
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
-
-// setting static files for css
 app.use(express.static('public'))
-
-// setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
-// setting mongoose
 require('./config/mongoose')
-
-// 引用 routes
+usePassport(app)
 app.use(routes)
 
 //start and listen on the Express server
